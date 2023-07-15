5bd7e7e6d6deae494f245bec6a2106d954e9bf98
Shuhei Ohno <ohno.github@gmail.com>
2023-07-16 02:59:29 +0900

    add lower-case

--- assets/js/sci2dec.js
+++ assets/js/sci2dec.js
@@ -1,7 +1,7 @@
 function sci2dec(input) {
   let output = input
   // https://developer.mozilla.org/ja/docs/Web/JavaScript/Guide/Regular_expressions/Groups_and_backreferences
-  const regexpNames =  /(?<mantissa>[+-]?\d+(?:\.\d+)?)[ED]+(?<exponent>[+-]?\d+)/mg;
+  const regexpNames =  /(?<mantissa>[+-]?\d+(?:\.\d+)?)[EDed]+(?<exponent>[+-]?\d+)/mg;
   console.log("hello")
   for (const match of input.matchAll(regexpNames)) {
     // console.log(match[0])
